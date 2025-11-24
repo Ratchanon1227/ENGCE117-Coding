230f1f75f994d1385eb801c31c3bd9b63295db6f
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-11-24 19:00:47 +0700

    Add files via upload

--- 1.2.c
+++ 1.2.c
@@ -15,10 +15,13 @@ int main(){
         else if(score >= 70){
             printf("Grade = B ");
         }
-        else if(score >= 65){
+        else if(score >= 60){
             printf("Grade = C ");
         }
-        else if(score <= 49){
+        else if(score >= 50){
+            printf("Grade = D ");
+        }
+        else if(score < 50){
             printf("Grade = F ");
         }
             return 0; 
