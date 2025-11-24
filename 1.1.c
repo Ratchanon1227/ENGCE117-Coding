cccadba2d460732936f1f58c4466ea79ab8f3e1e
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-11-24 17:58:15 +0700

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,21 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
+
+int main() {
+    int N, i;
+
+    // รับค่า N (จำนวนรอบ)
+    if (scanf("%d", &N ) != 1 || N <= 0 ) {
+        printf( "Error\n" ) ; 
+        return 1; // Handle input failure
+    }
+    else {
+        for(i = 0 ; i < N ; i++ ) {
+            printf( "Hello Loop!\n") ;
+        }
+    }
+    
+    // TODO: Implement the for loop to print the message N times
+
+    return 0;
+}
