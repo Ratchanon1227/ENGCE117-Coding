b1042aed9e15fc28c525e75f8193e84b3fb90701
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-12-12 15:47:08 +0700

    Add files via upload

---
+++ 2.4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float principal, monthlyPayment, interest ;
+    int monthCount = 0 ;
+    const float INTEREST_RATE = 0.01 ;
+    const float PENALTY = 10.0 ;
+
+    if(scanf("%f %f", &principal, &monthlyPayment) !=2 ) {
+        return 1 ;
+    }
+
+    while (principal > 0) {
+        monthCount++ ;
+        interest = principal * INTEREST_RATE ;
+        principal = principal + interest ;
+
+        if(monthlyPayment < interest) {
+            principal = principal + PENALTY ;
+        }
+        principal = principal - monthlyPayment ;
+        printf("Month %d : Remaining = %.2f\n", monthCount, principal) ;
+    }
+    
+    printf("Loan Settled in : %d Month.\n", monthCount) ;
+    return 0;
+}
