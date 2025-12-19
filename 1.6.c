a62f6a51edb4642a7c434bec203f08ebd07d0993
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-12-19 19:59:47 +0700

    Add files via upload

--- 1.6.c
+++ 1.6.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    int number;
+int main() {
+    int number ;
 
-    do{
-        printf("Enter a Number (1-10) : ");
+    do {
+        printf("Enter a number (1-10): ") ;
+        
         if (scanf("%d", &number) != 1) {
-            break;
-            }
-        if (number < 1 || number > 10 ){
-            printf("Error : Value Must be 1-10 \n");
+            break; 
         }
-    }
-    while (number < 1 || number > 10 );
-    {
-      printf("Input accepted : %d\n ", number);
-      return 0;
-    }
+        if (number < 1 || number > 10) {
+            printf("Error: Value must be 1-10.\n") ;
+        }
+
+    } 
+    while (number < 1 || number > 10) ;
+
+    printf("Input accepted: %d\n", number) ;
+
+    return 0;
 }
