7bc22df462c45fe0f860235f5292c2c31ae3c78c
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-12-19 20:05:21 +0700

    Add files via upload

--- 1.7.c
+++ 1.7.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 
+int main() {
+    int customerType ;
+    float consumption_kWh ;
+    float totalBill = 0.0 ;
 
-int main(){
-    int customerType;
-    float consumption_kwh;
-    float totalBill = 0.0;
+    if (scanf("%d %f", &customerType, &consumption_kWh) != 2) 
+        return 1 ;
 
-
-    printf("เลือกหัวข้อของคุณ (1.ครัวเรือน or 2.ธุรกิจ):");
-    if(scanf("%d %f", &customerType, &consumption_kwh) != 2){
-        return 1;
-    }
-    if(customerType == 1){
-        if(consumption_kwh >=0 && consumption_kwh <=100){
-            totalBill = consumption_kwh * 3.0;
-        }else{
-            totalBill = consumption_kwh * 4.0;
+    if (customerType == 1) {
+        if (consumption_kWh <= 100) {
+            totalBill = consumption_kWh * 3.0 ;
+        } 
+        else {
+            totalBill = consumption_kWh * 4.0 ;
         }
-    } else if (customerType == 2){
-        if(consumption_kwh >=0 && consumption_kwh <=500){
-            totalBill = consumption_kwh * 5.0;
-        }else{
-            totalBill = consumption_kwh * 6.5;
+    } 
+    else if (customerType == 2) {
+        if (consumption_kWh <= 500) {
+            totalBill = consumption_kWh * 5.0 ;
+        } 
+        else {
+            totalBill = consumption_kWh * 6.5 ;
         }
-    } else {
-        printf("ไม่มีรายการนี้\n");
-        totalBill = -1.0;
+    } 
+        else {
+        printf("Invalid Customer Type\n");
+        return 0;
     }
-    if (totalBill >= 0.0){
-        printf("ค่าใช้จ่ายรวม : %.2f บาท\n", totalBill);
-    }
-    if(totalBill >= 0.0 ){
-        printf("Total Bill: %.2f Bath\n", totalBill);
-    }
+
+    printf("%.2f\n", totalBill);
     return 0;
 }
