494a8336c0050384ea591ecf0c052052c289440a
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-12-20 11:23:26 +0700

    Add files via upload

---
+++ lab/Lab 3.3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct student {
+    char name[20] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;
+
+
+void upgrade(struct student *child) {
+    if (child->sex == 'M') {
+        child->gpa = child->gpa * 1.10 ;
+    } 
+        else if (child->sex == 'F') {
+            child->gpa = child->gpa * 1.20 ;
+    }
+}
+
+int main() {
+    struct student aboy ;
+    struct student agirl ;
+    aboy.sex = 'M' ;
+    aboy.gpa = 3.00 ;
+    agirl.sex = 'F' ;
+    agirl.gpa = 3.00 ;
+    upgrade(&aboy) ;
+    upgrade(&agirl) ;
+    printf("Aboy GPA (+10%%) : %.2f\n", aboy.gpa) ;
+    printf("Agirl GPA (+20%%) : %.2f\n", agirl.gpa) ;
+    return 0;
+}
