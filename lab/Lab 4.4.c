071b581d38d922ed95032ed941b13a23c43985e9
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2025-12-26 17:08:20 +0700

    Add files via upload

---
+++ lab/Lab 4.4.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20] ;
+    int age ;
+    char sex ;
+    float gpa ;
+    struct studentNode *next ;
+} ;
+
+void SaveNode(struct studentNode *child, char n[], int a, char s, float g) ;
+void GoNext1(struct studentNode **walk) ;
+
+int main() {
+    struct studentNode *start, *now1, **now2 ;
+    
+    start = (struct studentNode*)malloc(sizeof(struct studentNode)) ;
+    SaveNode(start, "TK", 21, 'M', 3.03) ;
+
+    start->next = (struct studentNode*)malloc(sizeof(struct studentNode)) ;
+    SaveNode(start->next, "MT", 24, 'F', 3.44) ;
+
+    start->next->next = (struct studentNode*)malloc(sizeof(struct studentNode)) ;
+    SaveNode(start->next->next, "Art", 21, 'M', 3.23) ;
+
+    start->next->next->next = (struct studentNode*)malloc(sizeof(struct studentNode)) ;
+    SaveNode(start->next->next->next, "Bas", 21, 'M', 3.55) ;
+
+    now1 = start ;
+    now2 = &start ;
+
+    GoNext1(&now1) ;
+    printf("%s ", now1->name) ;
+
+    return 0;
+}
+
+void SaveNode(struct studentNode *child, char n[], int a, char s, float g) {
+    strcpy(child->name, n) ;
+    child->age = a ;
+    child->sex = s ;
+    child->gpa = g ;
+    child->next = NULL ;
+}
+
+void GoNext1(struct studentNode **walk) {
+    while (*walk != NULL) {
+        printf("Name : %s |", (*walk)->name);
+        printf("Age : %d |", (*walk)->age);
+        printf("Sex : %c |", (*walk)->sex);
+        printf("GPA : %.2f \n", (*walk)->gpa);
+        printf("------------\n");
+        *walk = (*walk)->next;
+    }
+}
