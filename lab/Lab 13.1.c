18d7576578b1a9eb88f82584399d860b6c97ec25
Ratchanon Wong-in <ratchanonwongin@gmail.com>
2026-03-06 16:48:56 +0700

    Add files via upload

--- lab/Lab 13.1.c
+++ lab/Lab 13.1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int max(int a, int b) { return (a > b) ? a : b; }
 int *KnapsackDP(int *w, int *v, int n, int wx) {
+    static int x[100] ; 
     int K[n + 1][wx + 1] ;
 
+    for (int i = 0; i < n; i++) x[i] = 0 ;
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= wx; j++) {
             if (i == 0 || j == 0)
@@ -16,7 +17,6 @@ int *KnapsackDP(int *w, int *v, int n, int wx) {
         }
     }
 
-    int *x = (int *)calloc(n, sizeof(int)) ;
     int res = K[n][wx] ;
     int j = wx ;
     for (int i = n; i > 0 && res > 0; i--) {
@@ -34,10 +34,7 @@ int main() {
     int w[5] = { 1, 2, 5, 6, 7 } ;
     int v[5] = { 1, 6, 18, 22, 28 } ;
     int *x ;
-
     x = KnapsackDP(w, v, n, wx) ;
     for (int i = 0; i < n; i++) printf("%d ", x[i]) ;
-
-    free(x) ;
     return 0 ;
 }
